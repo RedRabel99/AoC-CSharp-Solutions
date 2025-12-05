3fb7800f1764ec55c853bf2d85b5ee6cdc5f9d54
Paweł Lysko <lyskopawel03@gmail.com>
2025-12-05 13:55:09 +0100

    Add 2025 day 5 part 1 solution

---
+++ src/AoC2025/Day05.cs
@@ -0,0 +1,39 @@
+﻿using Common;
+
+namespace AoC2025;
+
+public class Day05 : BaseLibraryDay
+{
+    protected override int Year => 2025;
+    private string[] _input;
+    private long[][] ranges;
+    private long[] ids;
+
+    public Day05()
+    {
+        _input = File.ReadAllLines(InputFilePath);
+        ParseInput();
+    }
+
+    private void ParseInput()
+    {
+        var gap = _input.IndexOf("");
+        ranges = _input.Take(gap).Select(x => x.Split('-').Select(long.Parse).ToArray()).ToArray();
+        ids = _input.Skip(gap + 1).Select(long.Parse).ToArray();
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        var result = ids.Count(i => ranges.Any(r => r[0] <= i && r[1] >= i));
+
+        return new(result.ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        var result = 0;
+
+        return new(result.ToString());
+    }
+
+}
